97ffcb4a27b0e986de1cdc27129afeb593c0335d
Anthony Calabretta <me@arytonex.pw>
2018-07-29 04:02:56 +0200

    LrcUtils: strip useless tags and enhanced formats

--- LrcUtils.php
+++ LrcUtils.php
@@ -4,9 +4,13 @@ class LrcUtils{
 
     public static function textArr($rawText){
         if(is_null($rawText)) return null;
+        // remove LRC tags
+        $rawText = trim(preg_replace(["(\[ti:.*\])", "(\[ar:.*\])", "(\[au:.*\])", "(\[al:.*\])", "(\[re:.*\])", "(\[ve:.*\])", "(\[by:.*\])", "(\[length:.*\])", "(\[offset:.*\])"], "", $rawText));
+        // remove enhanced LRC format
+        $rawText = preg_replace("(<\d{2}:\d{2}\.\d{2}> )", "", $rawText);
         $rawText = explode("\n", $rawText);
         $newText = array();
-        foreach($rawText as $index => $line){
+        foreach($rawText as $line){
             $time = explode(":", str_replace(".", ":", substr($line, 1, 8)));
             $time = /*($time[2] / 100) + */ $time[1] + ($time[0] * 60);
             $verse = trim(substr($line, 10));
