caae0b637fbcd6c5823fa8390364248bc2fd8b33
Anthony Calabretta <me@arytonex.pw>
2018-07-29 04:04:09 +0200

    Providers: Add Baidu as lyrics provider (adapted from BreadPlayer)

---
+++ providers/Baidu.php
@@ -0,0 +1,47 @@
+<?php
+
+class Baidu extends Provider{
+
+    public const PROVIDER_PRIORITY = 1;
+
+    private const QUERY_URL = "http://tingapi.ting.baidu.com/v1/restserver/ting?from=qianqian&version=2.1.0&method=baidu.ting.search.merge&format=json&query={query}&page_no=1&page_size=5&type=-1&data_source=0&use_cluster=1";
+    private const LYRICS_URL = "http://tingapi.ting.baidu.com/v1/restserver/ting?from=qianqian&version=2.1.0&method=baidu.ting.song.lry&format=json&songid={songid}";
+
+    public function fetchLyrics($artist, $title){
+        $query = $this->getSongQuery($artist, $title);
+        if(isset($query) && !empty($query)){
+            foreach ($query as $result){
+                if($result["title"] == $title){
+                    $lyrics = $this->getLyricsFromSongId($result["song_id"]);
+                    if(is_string($lyrics) && $lyrics !== ""){
+                        if(isset($this->offlineHelper))
+                            $this->offlineHelper->saveLyrics($artist, $title, $lyrics);
+                        return $lyrics;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private function getSongQuery($artist, $title){
+        $url = str_replace("{query}", urlencode($title . " " . $artist), self::QUERY_URL);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true)["song_info"]["song_list"] ?? null;
+    }
+
+    private function getLyricsFromSongId($songId){
+        $url = str_replace("{songid}", urlencode($songId), self::LYRICS_URL);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true)["lrcContent"] ?? null;
+    }
+
+}
--- run.php
+++ run.php
@@ -31,6 +31,7 @@ include_once __DIR__ . "/OfflineHelper.php";
 include_once __DIR__ . "/PlayerCtl.php";
 include_once __DIR__ . "/providers/Provider.php";
 include_once __DIR__ . "/providers/Musixmatch.php";
+include_once __DIR__ . "/providers/Baidu.php";
 include_once __DIR__ . "/providers/OfflineProvider.php";
 
 $player = new PlayerCtl();
