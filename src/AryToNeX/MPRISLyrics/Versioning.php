7f670b925a525315262d415ff091b92cdff2c2cb
Anthony Calabretta <me@arytonex.pw>
2018-08-09 02:53:51 +0200

    Versioning: From now on I will work on beta2 (does this make sense?)

--- src/AryToNeX/MPRISLyrics/Versioning.php
+++ src/AryToNeX/MPRISLyrics/Versioning.php
@@ -7,7 +7,7 @@ class Versioning{
     public const VERSION_MAJOR = 1;
     public const VERSION_MINOR = 0;
     public const VERSION_PATCH = 0;
-    public const VERSION_NOTES = "beta1";
+    public const VERSION_NOTES = "beta2-dev";
 
     public static function getVersion() : string{
         return implode(".", [self::VERSION_MAJOR, self::VERSION_MINOR, self::VERSION_PATCH]) . "-" . self::VERSION_NOTES;
