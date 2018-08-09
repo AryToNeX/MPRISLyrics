3818b03ef2a4c983a419396df43333b4af52d301
Anthony Calabretta <me@arytonex.pw>
2018-08-09 02:50:42 +0200

    BuildPhar: rename and remove useless flags

--- BuildPhar.php
+++ BuildPhar.php
@@ -4,7 +4,7 @@ include_once "src/AryToNeX/MPRISLyrics/Versioning.php";
 $srcRoot = "src/";
 $buildRoot = "build/";
 $fileName = "MPRISLyrics-" . \AryToNeX\MPRISLyrics\Versioning::getVersion() . ".phar";
-$phar = new Phar($buildRoot . $fileName, FilesystemIterator::CURRENT_AS_FILEINFO|FilesystemIterator::KEY_AS_FILENAME, $fileName);
+$phar = new Phar($buildRoot . $fileName, null, $fileName);
 $phar->buildFromDirectory($srcRoot,'/.php$/');
 $phar->setStub($phar->createDefaultStub("AryToNeX/MPRISLyrics/Loader.php"));
 
