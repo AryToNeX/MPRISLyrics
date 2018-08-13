c470c3f05ca61df89377102b353687d11f21721f
Anthony Calabretta <me@arytonex.pw>
2018-08-13 03:17:52 +0200

    OfflineHelper: maybe we should sanitize filenames, shouldn't we?

--- src/AryToNeX/MPRISLyrics/OfflineHelper.php
+++ src/AryToNeX/MPRISLyrics/OfflineHelper.php
@@ -12,20 +12,29 @@ class OfflineHelper{
     }
 
     public function checkLyrics(string $artist, string $title) : bool{
-        return is_file($this->workdir . "/lyrics/$artist/$title.lrc");
+        return is_file($this->workdir . "/lyrics/".$this->sanitizeName($artist)."/".$this->sanitizeName($title).".lrc");
     }
 
     public function getLyrics(string $artist, string $title) : ?string{
         if($this->checkLyrics($artist, $title))
-            return file_get_contents($this->workdir . "/lyrics/$artist/$title.lrc");
+            return file_get_contents($this->workdir . "/lyrics/".$this->sanitizeName($artist)."/".$this->sanitizeName($title).".lrc");
         return null;
     }
 
     public function saveLyrics(string $artist, string $title, string $lyrics, bool $overwrite = false) : void{
         @mkdir($this->workdir . "/lyrics");
-        @mkdir($this->workdir . "/lyrics/$artist");
+        @mkdir($this->workdir . "/lyrics/" . $this->sanitizeName($artist));
         if(!$this->checkLyrics($artist, $title) || $overwrite)
-            file_put_contents($this->workdir . "/lyrics/$artist/$title.lrc", $lyrics);
+            file_put_contents($this->workdir . "/lyrics/".$this->sanitizeName($artist)."/".$this->sanitizeName($title).".lrc", $lyrics);
+    }
+
+    private function sanitizeName(string $unsanitizedName) : string{
+        $unsanitizedName = htmlentities($unsanitizedName, ENT_QUOTES, 'UTF-8');
+        $unsanitizedName = preg_replace('~&([a-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', $unsanitizedName);
+        $unsanitizedName = html_entity_decode($unsanitizedName, ENT_QUOTES, 'UTF-8');
+        $unsanitizedName = preg_replace("(<|>|\||\?|\*|\\$|:|\\\\|[\/]|\"|\\0|[\\1-\\37])", "_", $unsanitizedName);
+
+        return trim($unsanitizedName);
     }
 
 }
