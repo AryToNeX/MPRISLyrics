d58322c288eaff11ca42eca49469e2786c2d0835
Naomi Calabretta <me@arytonex.pw>
2019-08-12 04:43:38 +0200

    Fixed phar builder

--- BuildPhar.php
+++ BuildPhar.php
@@ -4,9 +4,19 @@ include_once "src/AryToNeX/MPRISLyrics/Versioning.php";
 $srcRoot = "src/";
 $buildRoot = "build/";
 @mkdir("build/");
+
 $fileName = "MPRISLyrics-" . \AryToNeX\MPRISLyrics\Versioning::getVersion() . ".phar";
+if(is_file($buildRoot . $fileName)) unlink($buildRoot . $fileName);
 $phar = new Phar($buildRoot . $fileName, null, $fileName);
-$phar->buildFromDirectory($srcRoot,'/.php$/');
+$phar->buildFromIterator(
+	new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator(
+			"src/AryToNeX/MPRISLyrics",
+			RecursiveDirectoryIterator::SKIP_DOTS
+		)
+	),
+	"src/"
+);
 $phar->setStub($phar->createDefaultStub("AryToNeX/MPRISLyrics/Loader.php"));
 
 echo "Phar created.\n";
